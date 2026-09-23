388be85435a6b07f28cb66898c44b3ced475313d
Koffi Elom AYISSOU <technodreyfusinfo@gmail.com>
2026-09-23 13:09:08 +0000

    fix: correct teacher names migration

--- drizzle/0124_add_teacher_names.sql
+++ drizzle/0124_add_teacher_names.sql
@@ -1,2 +1,2 @@
 ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "last_name" text;
-ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "first_names" text;
+ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "first_name" text;
